de0acabf33afbab459fc9185ab2bff1c5b8ffafb
athosmartinez <athosandrade04@gmail.com>
2024-11-25 22:45:45 -0300

    remove services

--- services/maps-images-generator/routes/imageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const imageController = require("../controllers/imageController");
-
-// Rota POST para capturar imagens
-router.post("/", imageController.capturarImagens);
-
-module.exports = router;
